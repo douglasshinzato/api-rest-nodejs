9f92b957a4234b2a71ab7febf0fc415ef4f1c0c0
Douglas Shinzato <shinzato.douglas@gmail.com>
2024-09-17 13:18:29 -0400

    Initial commit

---
+++ src/routes/transactions.ts
@@ -0,0 +1,89 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import crypto from 'node:crypto'
+import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  //Rota para listar todas as transações.
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    //Quanto não conseguia listar as rotas, o problema foi que criei uma variavel dentro do cookie chamada "session_Id", e na hora de recuperar essa variavel na listagem eu tentava buscar dentro do cookie uma variavel chamada "sessionId".
+    const { sessionId } = request.cookies
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
+
+    return { transactions }
+  })
+
+  //Rota para listar uma transação específica.
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const { sessionId } = request.cookies
+
+    const transaction = await knex('transactions')
+      .where({
+        //"shorthand" para colocar duas condições no "where".
+        session_id: sessionId,
+        id,
+      })
+      .first()
+
+    return { transaction }
+  })
+
+  // Rota para mostrar o resumo das transações.
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      const summary = await knex('transactions')
+        .where('session_id', sessionId)
+        .sum('amount', { as: 'amount' })
+        .first()
+
+      return { summary }
+    }
+  )
+
+  //Rota para criar uma nova transação.
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    )
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days.
+      })
+    }
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
+    })
+
+    return reply.status(201).send()
+  })
+}
